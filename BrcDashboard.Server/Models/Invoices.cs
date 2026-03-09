2b99f7cb241698e43f0edda2159adf29d445a732
Lauren Dwyer <76599666+loldwyer@users.noreply.github.com>
2026-03-09 14:41:03 +0000

    Added invoices API controller and model

---
+++ BrcDashboard.Server/Models/Invoices.cs
@@ -0,0 +1,13 @@
+﻿namespace BrcDashboard.Server.Models
+{
+    public class Invoices
+    {
+        public int id { get; set; }
+        public string invoice_number { get; set; } = "";
+        public string customer_name { get; set; } = "";
+        public decimal amount { get; set; }
+        public DateTime due_date { get; set; }
+        public DateTime issue_date { get; set; }
+        public string status { get; set; } = "";
+    }
+}
